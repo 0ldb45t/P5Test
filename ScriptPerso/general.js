dcc363342c0afbf697a318625fa6df71d01b0eed
0ldb45t <bontempsbastien@gmail.com>
2025-04-12 15:07:13 -0400

    tntative Bordelique avec des trucs à sauver

--- ScriptPerso/general.js
+++ ScriptPerso/general.js
@@ -2,10 +2,7 @@
 const radiosForme = document.querySelectorAll(".forme");
 radiosForme.forEach((bouton) => bouton.addEventListener("click", selectionner));
 const radiosTaille = document.querySelectorAll(".taille");
-const radiosRotation = document.querySelectorAll(".rotation");
-radiosRotation.forEach((bouton) =>
-	bouton.addEventListener("click", selectionner)
-);
+
 let taille = 0;
 let forme = "";
 let rotation = 0;
@@ -13,38 +10,35 @@ let rotationGlobale = 0;
 let daron;
 let s;
 let taillePrinted = 0;
-let formePrinted = "";
+let formePrinted = {
+	"carre": p5.square,
+	"cercle": p5.circle,
+	"rectangle": p5.rect,
+};
 radiosTaille.forEach((bouton) =>
 	bouton.addEventListener("click", selectionner)
 );
 
 function selectionner(e) {
 	radiosForme.forEach((bouton) =>
+		bouton.className == e.srcElement.className &&
 		bouton.value != e.srcElement.value
 			? (bouton.checked = false)
 			: (forme = e.srcElement.value)
 	);
+
 	radiosTaille.forEach((bouton) =>
+		bouton.className == e.srcElement.className &&
 		bouton.value != e.srcElement.value
 			? (bouton.checked = false)
 			: (taille = e.srcElement.value)
 	);
-
-	e.srcElement.className === "rotation"
-		? (rotation = e.srcElement.value)
-		: "";
-
 	if (taille !== 0 && forme !== "") {
 		daron = document.getElementById("defaultCanvas0");
 		daron ? "" : setup();
 		taillePrinted = taille;
-		formePrinted = forme;
+		formePrinted[forme] = forme;
 		taille = 0;
-		forme = "";
 		draw();
 	}
-}
-function faireTourner(e) {
-	console.log("tourne!");
-	rotationGlobale += 10;
 }
--- sketch.js
+++ sketch.js
@@ -9,22 +9,29 @@ function setup() {
 	createCanvas(700, 700);
 	angleMode("degrees");
 }
-/*print la vieille versiobn avant de redessiner */
-/*let sketchCarre = function (sketch) {
-	// if (daron) {
-	// 	/*sketch.save(sketch.canvas, "canva");
-	// 	sketch.clear();
-	// 	sketch.print("canva");*/
-// 	sketch.createCanvas(700, 700);
+
+let fct;
+
+// switch (formePrinted) {
+// 	case "carre":
+// 		fct = square;
+// 		break;
+// 	case "cercle":
+// 		fct = circle;
+// 		break;
+// 	case "rectangle":
+// 		fct = rect;
+// 		break;
+// 	default:
+// 		break;
 // }
-// sketch.setup = function () {
-// 	//sketch.clear();
-// 	sketch.createCanvas(700, 700);
-// 	sketch.angleMode("degrees");
-// 	console.log(sketch.canvas);
-// 	//test.parent("dessin");
-// };
 function draw() {
+	fct = formePrinted.forme;
+	forme = "";
+	drawTest(fct);
+}
+function drawTest(fct) {
+	if (fct === undefined) return;
 	if (mouseIsPressed === true) {
 		fill(114);
 		rotationGlobale++;
@@ -35,20 +42,5 @@ function draw() {
 		rotate(rotationGlobale);
 		fill(255);
 	}
-	switch (formePrinted) {
-		case "carre":
-			square(0, 0, taillePrinted);
-			break;
-		case "cercle":
-			circle(0, 0, taillePrinted);
-			break;
-		case "rectangle":
-			rect(0, 0, taillePrinted, taillePrinted - 50);
-			break;
-		default:
-			break;
-	}
-
-	//rotation != 0 ? sketch.rotate(rotationGlobale) : (rotation = 0);
-	//sketch.square(sketch.mouseX, sketch.mouseY, taille);
+	fct(0, 0, taillePrinted, taillePrinted - 50);
 }
